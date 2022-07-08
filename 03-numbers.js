09678811974bd5a0376e592f72206fae9b79a086
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-08 23:51:30 +0300

    Завдання репета + автоперевірка до 13 завдання

---
+++ 03-numbers.js
@@ -0,0 +1,9 @@
+/* Парс числа с Number.parseInt
+ */
+
+// let elementWidth = '50px'
+// const result =  Number.parseInt(elementWidth);
+
+// console.log(result);
+
+
---
+++ auto-check.js
@@ -0,0 +1,121 @@
+
+/* Завдання 2 автоперевірка
+ */
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// productName = "Repair droid";
+// pricePerItem += 1500;
+
+// console.log(pricePerItem); 
+// console.log(productName);
+
+
+
+/* Завдання 3 автоперевірка
+ */
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+
+
+/* Завдання 4 автоперевірка
+ */
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+
+
+/* Завдання 5 автоперевірка
+ */
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+
+
+/* Завдання 6 автоперевірка
+ */
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee ;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+// console.log(totalPrice);
+// console.log(message);
+
+
+
+/* Завдання 7 автоперевірка
+ */
+// function sayHi() {
+//     console.log("Hello, this is my first function!")
+// }
+// sayHi();
+
+
+
+/* Завдання 8 автоперевірка
+ */
+// function add(a, b, c,) {
+//     console.log(`Addition result equals ${a + b + c}`);
+//   }
+  
+//   add(15, 27, 10);
+//   add(10, 20, 30);
+//   add(5, 10, 15);
+
+
+
+/* Завдання 9 автоперевірка
+ */
+// function add(a, b, c) {
+//   return a + b + c;
+//   }
+  
+//   add(2, 5, 8); // 15
+//   let result = add(2, 5, 8);
+//   console.log(result);
+  
+//   console.log(add(15, 27, 10));
+//   console.log(add(10, 20, 30));
+//   console.log(add(5, 10, 15));
+
+
+
+/* Завдання 10 автоперевірка
+ */
+// function makeMessage (name, price) {
+    
+//     const message = `You picked ${name}, price per item is ${price} credits.`;
+
+//     return message;
+//   };
+
+/* Завдання 12 автоперевірка
+ */
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+   
+//   const makeOrderMessage = `You ordered droids worth ${orderedQuantity * pricePerDroid + deliveryFee} credits. Delivery (${deliveryFee} credits) is included in total price.`
+   
+//     return makeOrderMessage;
+//     makeOrderMessage()
+//   }
+
+
+/* Завдання 13 автоперевірка
+ */
+
