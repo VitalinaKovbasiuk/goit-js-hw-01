4c6bd7dbc5c38b5433e4e9b45a10f3c560959340
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-12 21:20:50 +0300

    Робота по відео Репети ч.4

--- 06-logical-oparators.js
+++ 06-logical-oparators.js
@@ -1,2 +1,34 @@
-/* Операторы сравнения
+/* Приведение в булю
  */
+// console.log(Number(null));
+// console.log(Number('0'));
+// console.log(Number(false));
+// !!!!!!! Заповните 6 ложных значений приводящих в false в логическом преобразовании. Все остальное приводится к true.
+// console.log(Boolean(0));
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(''));
+// console.log(Boolean(""));
+
+
+
+/* Логическое и
+ */
+// console.log(5 && 6 && 'hello')
+// если все значение истинны, то вернется последние.... ищет до первого false и запинается на лжи
+
+
+
+/* Логическое или
+ */
+// console.log(5 || null || 'hello' || 0);
+// console.log(false || 0 || null || 0);
+// если находит true показывает - если нигде не находит true возващает последние
+
+
+
+/* Логическое не оператор !
+ */
+// console.log(!false);
+// делает инверсию - преворот
